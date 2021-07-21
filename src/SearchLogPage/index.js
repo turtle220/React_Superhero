34d79cdaa6d8d1ea805076737f8556dcf3e76dd4
Denys <6eptpyda@gmail.com>
2021-07-22 03:38:31 +1000

    [Update]: customized the search page

--- src/SearchLogPage/index.js
+++ src/SearchLogPage/index.js
@@ -1,47 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BackImage from '../Image/button-back.svg'
+import './index.css'
 
 export default function SearchLog() {
+
   return (
     <>
-      <div style={{ paddingTop: '10%', paddingLeft: '5%' }}>
-        <a
-          href='/'
-          style={{
-            textDecoration: 'none',
-            display: 'flex',
-            fontWeight: 'bold',
-            color: '#8f8f8f',
-            fontSize: '24px',
-            cursor: 'pointer'
-          }}>
-          <img src={BackImage} alt='' />
-          <p style={{ paddingLeft: '1%', color: 'black' }}>Back</p>
-        </a>
-      </div>
-      <h1 style={{ textAlign: 'center', paddingBottom: '3%' }}>
+    <div style={{paddingTop: '3%', paddingLeft: '3%'}}>
+      <button onClick={() => window.location.replace("/")} className='back-button'>
+        <div style={{display: 'flex'}}>
+          <img src={BackImage} alt='' /> 
+          <p style={{color:'white', paddingLeft: '15%'}}>Back</p>
+        </div>
+      </button>
+    </div>
+      <h1 style={{ textAlign: 'center' }}>
         Search History
       </h1>
-      <table style={{ margin: 'auto', width: '40%' }}>
-        <thead>
-          <tr>
-            <th>NO</th>
-            <th style={{ textAlign: 'center' }}>Search Name</th>
-          </tr>
-        </thead>
-        {localStorage.getItem('inputName') &&
-          localStorage
-            .getItem('inputName')
-            .split(',')
-            .map((res, key) => {
-              return (
-                <tr>
-                  <td>{key}</td>
-                  <td style={{textAlign: 'center'}}>{res}</td>
-                </tr>
-              )
-            })}
-      </table>
+      <div className='limiter'>
+      <div className='container-table'>
+        <div style={{width: '600px'}}>
+          <div className='table-verb'>
+            <div className='table-thead'>
+              <table>
+                <thead>
+                  <tr className='row-head'>
+                    <th className='column' style={{width: '23%', paddingLeft: '5%'}}>No</th>
+                    <th className='column' style={{width: '22%'}}>Search Name</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div className='table-body'>
+              <table>
+                <tbody>
+                {localStorage.getItem('inputName') &&
+                  localStorage
+                    .getItem('inputName')
+                    .split(',')
+                    .map((res, key) => {
+                      return (
+                        <tr>
+                          <td style={{paddingLeft: '5%'}}>{key}</td>
+                          <td style={{paddingLeft: '20%'}}>{res}</td>
+                        </tr>
+                      )
+                    })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
